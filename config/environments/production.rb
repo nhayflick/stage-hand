f3e890b0e23f9d6049180658e4e568b01aa2b0f4
Nate Hayflick <nhayflick@gmail.com>
2013-11-27 18:29:49 -0800

    AWS Bucket Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ StageHand::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['stagehand-assets'], 
+      :bucket => ENV['AWS_BUCKET'], 
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
