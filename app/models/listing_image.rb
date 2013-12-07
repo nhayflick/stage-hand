31b590feeca694e18030266bae628cf8f97798ff
Nate Hayflick <nhayflick@gmail.com>
2013-12-07 12:32:46 -0800

    Bootstrapped Listings

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -1,12 +1,13 @@
 class ListingImage < ActiveRecord::Base
 
 	has_attached_file :image, :styles => {
-	    thumb: '-quality 80 100x100>',
-	    square: '-quality 80 200x200#',
-	    medium: '-quality 80 300x300>'
+	    thumb: '100x100#',
+	    square: '200x200#',
+	    medium: '300x300>'
 	}
 
-  	validates_attachment :image, :presence => true, :content_type => { :content_type => ["image/jpg", "image/png"] }, :size => { :in => 0..5.megabytes }
+  	validates_attachment :image, :presence => true, :size => { :in => 0..5.megabytes }
+  	validates_attachment_content_type :image, :content_type => /image/
 
   	belongs_to :listing
 
