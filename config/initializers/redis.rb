edafdcbe657e6aaf3c972056152445814843459d
Nate Hayflick <nhayflick@gmail.com>
2013-12-14 17:24:58 -0800

    More redis changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,9 +2,9 @@ uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => uri}
+  config.redis = { :url => ENV["REDISTOGO_URL"]}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => uri }
+  config.redis = { :url => ENV["REDISTOGO_URL"] }
 end
